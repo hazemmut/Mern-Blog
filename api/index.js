449cff30376b619bf63df19d75330d800e7e9a08
Gimini120 <alhasanhazem95@gmail.com>
2024-10-11 15:25:08 +0300

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,25 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+
 
 const app = express();
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("database  connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-app.listen(3000, (req,res,next) => {
-    console.log('Server is started!!! ');
-    
-})
+app.listen(3000, (req, res, next) => {
+  console.log("Server is started!!! ");
+});
+
+// KniP3WxOQ9BKS4gr
+
+// mongodb+srv://hazeeemm5566:<db_password>@mern-blog.3xxjd.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog
