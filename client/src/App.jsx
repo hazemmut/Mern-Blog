a789668c5cd4dc0e68e7382dacdb3874b6a4eef9
Gimini120 <alhasanhazem95@gmail.com>
2024-10-22 14:26:57 +0300

    create footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
  
 
@@ -25,6 +26,7 @@ export default function App() {
 
     
       </Routes>
+      <Footer />
       </BrowserRouter>
     </div>
   )
